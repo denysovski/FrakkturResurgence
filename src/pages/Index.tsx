7802a7b25246e616188baf95001b9ac07cd58514
Daniel Svoboda <dankosvobodkaaaa@gmail.com>
2026-02-21 15:50:54 +0100

    Wire locale props in Index to prevent runtime crash

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import Navbar from "@/components/Navbar";
 import HeroCarousel from "@/components/HeroCarousel";
 import Sections from "@/components/Sections";
 import NewsletterPopup from "@/components/NewsletterPopup";
 
+const countryOptions = [
+  { value: "GREAT BRITAIN", flag: "https://flagcdn.com/w40/gb.png" },
+  { value: "UNITED STATES", flag: "https://flagcdn.com/w40/us.png" },
+  { value: "CZECHIA", flag: "https://flagcdn.com/w40/cz.png" },
+  { value: "SLOVAKIA", flag: "https://flagcdn.com/w40/sk.png" },
+  { value: "GERMANY", flag: "https://flagcdn.com/w40/de.png" },
+  { value: "JAPAN", flag: "https://flagcdn.com/w40/jp.png" },
+  { value: "CHINA", flag: "https://flagcdn.com/w40/cn.png" },
+];
+
+const currencyOptions = ["GBP", "USD", "CZK", "EUR", "EUR", "JPY", "CNY"];
+
 const Index = () => {
+  const [selectedCountry, setSelectedCountry] = useState(countryOptions[0].value);
+  const [selectedCurrency, setSelectedCurrency] = useState(currencyOptions[0]);
+
   return (
     <div className="min-h-screen bg-background">
-      <Navbar />
+      <Navbar
+        countryOptions={countryOptions}
+        currencyOptions={currencyOptions}
+        selectedCountry={selectedCountry}
+        selectedCurrency={selectedCurrency}
+        onCountryChange={setSelectedCountry}
+        onCurrencyChange={setSelectedCurrency}
+      />
       <HeroCarousel />
-      <Sections />
+      <Sections
+        countryOptions={countryOptions}
+        currencyOptions={currencyOptions}
+        selectedCountry={selectedCountry}
+        selectedCurrency={selectedCurrency}
+        onCountryChange={setSelectedCountry}
+        onCurrencyChange={setSelectedCurrency}
+      />
       <NewsletterPopup />
     </div>
   );
